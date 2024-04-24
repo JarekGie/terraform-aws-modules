ae384b8f442170c617511dc9588f5e41b7a21e28
Jarek Golab <jaroslaw.golab@beaconcure.com>
2024-04-24 07:54:10 +0200

    added outputs for remote state module

--- examples/outputs.tf
+++ examples/outputs.tf
@@ -1,11 +1,11 @@
 output "aws_caller_identity_id" {
-  value = data.aws_caller_identity.curent.account_id
+  value = module.remote_state.aws_caller_identity_id
 }
 output "bucket_id" {
   description = "The id of the bucket"
-  value       = aws_s3_bucket.terraform_state.id
+  value       = module.remote_state.bucket_id
 }
 output "bucket_arn" {
   description = "The arn of the bucket"
-  value       = aws_s3_bucket.terraform_state.arn
+  value       = module.remote_state.bucket_arn
 }
