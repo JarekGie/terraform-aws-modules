a17e67e2cd897b46566ceeb89cfbe8c77b2ba1c5
Jarek Golab <jaroslaw.golab@beaconcure.com>
2024-04-24 07:54:10 +0200

    added outputs for remote state module

--- modules/remote_state/variables.tf
+++ modules/remote_state/variables.tf
@@ -1,9 +1,14 @@
 variable "bucket" {
   description = "Unique name for bucket"
 }
+variable "prevent_destroy" {
+  type    = bool
+  default = false
+}
 variable "region" {
   description = "Default region for remote state bucket"
 }
 variable "dynamodb_table_name" {
   description = "Name for state lock dynamodb table"
 }
+
