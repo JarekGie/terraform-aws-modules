9f6ae7bc6bd4a2b916d03d68c3e9d72a4de6888d
Your Name <you@example.com>
2024-09-24 10:50:46 +0200

    added vriable

--- examples/network/main.tf
+++ examples/network/main.tf
@@ -7,7 +7,7 @@ module "vpc" {
   public_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets    = ["10.0.3.0/24", "10.0.4.0/24"]
   availability_zones = ["eu-central-1a", "eu-central-1b"]
-
+  enable_nat_gateway = true
   tags = {
     Environment = "production"
   }
