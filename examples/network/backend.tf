6170784f29667ed52cf302f8d16aecc57daa05f4
Jarek Golab <jaroslaw.golab@beaconcure.com>
2024-10-06 12:15:41 +0200

    cleaned remote_state folder

--- examples/network/backend.tf
+++ examples/network/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "613448424242-testowy-bucket-ernesta"
+    bucket         = "820242946329-testowy-bucket-ernesta"
     key            = "stg/terraform.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "testowy-bucket-ernesta"
